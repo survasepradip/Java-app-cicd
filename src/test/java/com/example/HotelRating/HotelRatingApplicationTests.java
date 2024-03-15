d184831d0d94f47014336c72b6bc2b2ae7f4d684
Pradip Survase <90832128+survasepradip@users.noreply.github.com>
2024-03-16 03:06:54 +0530

    Update HotelRatingApplicationTests.java

--- src/test/java/com/example/HotelRating/HotelRatingApplicationTests.java
+++ src/test/java/com/example/HotelRating/HotelRatingApplicationTests.java
@@ -2,16 +2,13 @@ package com.example.HotelRating;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
-@ActiveProfiles("test")
-
 class HotelRatingApplicationTests {
 
 	@Test
-	void contextLoads(Application Context) {
+	void contextLoads() {
 		assertThat(context).isNotNull();
 	}
 
